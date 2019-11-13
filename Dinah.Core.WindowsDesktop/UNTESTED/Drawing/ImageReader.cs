49e398a99bbfd67aa8933dbe2cc611ca91acae2c
Robert McRackan <rmcracka@itron.com>
2019-11-13 08:35:46 -0500

    improved image conversion

--- Dinah.Core.WindowsDesktop/UNTESTED/Drawing/ImageReader.cs
+++ Dinah.Core.WindowsDesktop/UNTESTED/Drawing/ImageReader.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Dinah.Core.Drawing
 {
-    public static class ImageConverter
+	/// <summary>
+	/// The name ImageConverter is already taken by System.Drawing
+	/// </summary>
+	public static class ImageReader
     {
         // these seemingly intuitive ways of loading an image will lock the file
         // - with a stream
@@ -19,6 +23,15 @@ namespace Dinah.Core.Drawing
 		{
 			using var memoryStream = new System.IO.MemoryStream(bytes);
 			return Image.FromStream(memoryStream);
+		}
+
+		public static byte[] ToBytes(this Image image, ImageFormat format)
+		{
+			using var ms = new System.IO.MemoryStream();
+			{
+				image.Save(ms, format);
+				return ms.ToArray();
+			}
 		}
 	}
 }
