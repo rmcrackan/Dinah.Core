0dffe4adbf09d8649186a193c5a1ea27bd4f54c8
Robert McRackan <rmcracka@itron.com>
2019-12-05 16:02:09 -0500

    DataGridView extensions

--- Dinah.Core.WindowsDesktop/UNTESTED/Windows.Forms/DataGridViewExt.cs
+++ Dinah.Core.WindowsDesktop/UNTESTED/Windows.Forms/DataGridViewExt.cs
@@ -9,5 +9,17 @@ namespace Dinah.Core.Windows.Forms
 	{
 		public static IEnumerable<DataGridViewRow> AsEnumerable(this DataGridView dataGridView)
 			=> dataGridView.Rows.Cast<DataGridViewRow>();
-	}
+
+        public static int GetRowIdOfBoundItem<T>(this DataGridView dataGridView, Func<T, bool> func)
+        {
+            for (var r = 0; r < dataGridView.RowCount; r++)
+                if (func(dataGridView.GetBoundItem<T>(r)))
+                    return r;
+
+            return -1;
+        }
+
+        public static T GetBoundItem<T>(this DataGridView dataGridView, int rowIndex)
+            => (T)dataGridView.Rows[rowIndex].DataBoundItem;
+    }
 }
