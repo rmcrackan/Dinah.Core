e63fd7858a978bc42f113a64a6b9beae084c4415
Robert McRackan <rmcracka@itron.com>
2021-07-01 11:45:59 -0400

    Move UnicodeToAscii to Dinah.Core

--- Dinah.Core/StringExtensions.cs
+++ Dinah.Core/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Dinah.Core.Collections.Generic;
 
 namespace Dinah.Core
@@ -207,6 +208,82 @@ namespace Dinah.Core
 
 			var output = sb.ToString();
 			return output;
+		}
+
+		/// <summary>
+		/// Attempts to convert unicode characters to an approximately equal ASCII character.
+		/// </summary>
+		public static string UnicodeToAscii(this string unicodeStr)
+		{
+			// This method belongs in StringExtensions not RegexExtensions; regex is an implementation detail.
+			// This code has not been tested for performance
+
+			//Accents
+			unicodeStr = Regex.Replace(unicodeStr, "[éèëêð]", "e");
+			unicodeStr = Regex.Replace(unicodeStr, "[ÉÈËÊ]", "E");
+			unicodeStr = Regex.Replace(unicodeStr, "[àâä]", "a");
+			unicodeStr = Regex.Replace(unicodeStr, "[ÀÁÂÃÄÅ]", "A");
+			unicodeStr = Regex.Replace(unicodeStr, "[àáâãäå]", "a");
+			unicodeStr = Regex.Replace(unicodeStr, "[ÙÚÛÜ]", "U");
+			unicodeStr = Regex.Replace(unicodeStr, "[ùúûüµ]", "u");
+			unicodeStr = Regex.Replace(unicodeStr, "[òóôõöø]", "o");
+			unicodeStr = Regex.Replace(unicodeStr, "[ÒÓÔÕÖØ]", "O");
+			unicodeStr = Regex.Replace(unicodeStr, "[ìíîï]", "i");
+			unicodeStr = Regex.Replace(unicodeStr, "[ÌÍÎÏ]", "I");
+			unicodeStr = Regex.Replace(unicodeStr, "[š]", "s");
+			unicodeStr = Regex.Replace(unicodeStr, "[Š]", "S");
+			unicodeStr = Regex.Replace(unicodeStr, "[ñ]", "n");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ñ]", "N");
+			unicodeStr = Regex.Replace(unicodeStr, "[ç]", "c");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ç]", "C");
+			unicodeStr = Regex.Replace(unicodeStr, "[ÿ]", "y");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ÿ]", "Y");
+			unicodeStr = Regex.Replace(unicodeStr, "[ž]", "z");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ž]", "Z");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ð]", "D");
+
+			//Ligatures
+			unicodeStr = Regex.Replace(unicodeStr, "[œ]", "oe");
+			unicodeStr = Regex.Replace(unicodeStr, "[Œ]", "Oe");
+			unicodeStr = Regex.Replace(unicodeStr, "[ꜳ]", "aa");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ꜳ]", "AA");
+			unicodeStr = Regex.Replace(unicodeStr, "[æ]", "ae");
+			unicodeStr = Regex.Replace(unicodeStr, "[Æ]", "AE");
+			unicodeStr = Regex.Replace(unicodeStr, "[ꜵ]", "ao");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ꜵ]", "AO");
+			unicodeStr = Regex.Replace(unicodeStr, "[ꜷ]", "au");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ꜷ]", "AU");
+			unicodeStr = Regex.Replace(unicodeStr, "[«»ꜹꜻ]", "av");
+			unicodeStr = Regex.Replace(unicodeStr, "[«»ꜸꜺ]", "AV");
+			unicodeStr = Regex.Replace(unicodeStr, "[🙰]", "et");
+			unicodeStr = Regex.Replace(unicodeStr, "[ﬀ]", "ff");
+			unicodeStr = Regex.Replace(unicodeStr, "[ﬃ]", "ffi");
+			unicodeStr = Regex.Replace(unicodeStr, "[ﬄ]", "f‌f‌l");
+			unicodeStr = Regex.Replace(unicodeStr, "[ﬁ]", "fi");
+			unicodeStr = Regex.Replace(unicodeStr, "[ﬂ]", "fl");
+			unicodeStr = Regex.Replace(unicodeStr, "[ƕ]", "hv");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ƕ]", "Hv");
+			unicodeStr = Regex.Replace(unicodeStr, "[℔]", "lb");
+			unicodeStr = Regex.Replace(unicodeStr, "[ꝏ]", "oo");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ꝏ]", "OO");
+			unicodeStr = Regex.Replace(unicodeStr, "[ﬆ]", "st");
+			unicodeStr = Regex.Replace(unicodeStr, "[ꜩ]", "tz");
+			unicodeStr = Regex.Replace(unicodeStr, "[Ꜩ]", "TZ");
+			unicodeStr = Regex.Replace(unicodeStr, "[ᵫ]", "ue");
+			unicodeStr = Regex.Replace(unicodeStr, "[ꭣ]", "uo");
+
+			//Punctuation
+			unicodeStr = Regex.Replace(unicodeStr, "[«»\u2018\u2019\u201A\u201B\u2032\u2035]", "\'");
+			unicodeStr = Regex.Replace(unicodeStr, "[«»\u201C\u201D\u201E\u201F\u2033\u2036]", "\"");
+			unicodeStr = Regex.Replace(unicodeStr, "[\u2026]", "...");
+			unicodeStr = Regex.Replace(unicodeStr, "[\u1680]", "-");
+
+			//Spaces
+			unicodeStr = Regex.Replace(unicodeStr, "[«»\u00A0\u2000\u2002\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u200F\u205F\u3000]", " ");
+			unicodeStr = Regex.Replace(unicodeStr, "[«»\u2001\u2003]", "  ");
+			unicodeStr = Regex.Replace(unicodeStr, "[«»\u180E\u200B\uFEFF]", "");
+
+			return unicodeStr;
 		}
 	}
 }
