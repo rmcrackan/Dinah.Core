fd955d77b5113a1c6851a958e11a669c9d2fd9eb
Robert McRackan <rmcracka@itron.com>
2021-07-20 14:07:49 -0400

    New "Go.To" class

---
+++ Dinah.Core/Go.To.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Dinah.Core
+{
+    public static class Go
+    {
+        public static class To
+        {
+            // from: https://stackoverflow.com/a/43232486
+            public static void Url(string url)
+            {
+                try
+                {
+                    Process.Start(url);
+                }
+                catch
+                {
+                    // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        url = url.Replace("&", "^&");
+                        Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        Process.Start("xdg-open", url);
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    {
+                        Process.Start("open", url);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            /// <summary>Open folder, select file. If a folderPath is a folder: open parent folder, select folder</summary>
+            /// <param name="folderPath"></param>
+            public static void File(string folderPath) => Process.Start("explorer.exe", $"/select, \"{folderPath}\"");
+
+            /// <summary>Open folder</summary>
+            public static void Folder(string folderPath) => Process.Start("explorer.exe", $"\"{folderPath}\"");
+        }
+    }
+}
