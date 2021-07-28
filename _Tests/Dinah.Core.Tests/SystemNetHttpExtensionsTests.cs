9f0a5ac8f855a6841e0caa8943a2df53d235ca8b
Robert McRackan <rmcracka@itron.com>
2021-07-28 18:03:10 -0400

    remove tests of deleted code

--- _Tests/Dinah.Core.Tests/SystemNetHttpExtensionsTests.cs
+++ _Tests/Dinah.Core.Tests/SystemNetHttpExtensionsTests.cs
@@ -1,7 +1,4 @@
-﻿//// uncomment to run these tests
-//#define RUN_LIVE_DOWNLOADS
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -188,59 +185,6 @@ namespace SystemNetHttpExtensionsTests
             await Assert.ThrowsExceptionAsync<ArgumentException>(() => SystemNetHttpExtensions.DownloadFileAsync(mock, "url", ""));
             await Assert.ThrowsExceptionAsync<ArgumentException>(() => SystemNetHttpExtensions.DownloadFileAsync(mock, "url", "   "));
         }
-
-#if !RUN_LIVE_DOWNLOADS
-		[Ignore]
-#endif
-        [TestMethod]
-        public async Task download_small()
-        {
-            var downloadFileUrl = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
-            var length = 13264L;
-            var md5 = "d41d8cd98f00b204e9800998ecf8427e";
-
-            await downloadTest(downloadFileUrl, length, md5);
-        }
-
-#if !RUN_LIVE_DOWNLOADS
-        [Ignore]
-#endif
-        [TestMethod]
-        public async Task download_big()
-        {
-            var downloadFileUrl = "http://www.ovh.net/files/10Mb.dat";
-            var length = 1_250_000L;
-            var md5 = "d41d8cd98f00b204e9800998ecf8427e";
-
-            await downloadTest(downloadFileUrl, length, md5);
-        }
-
-        private static async Task downloadTest(string downloadFileUrl, long length, string md5)
-        {
-            var temp = Path.GetTempFileName();
-
-            try
-            {
-                // REAL LIVE download client
-                var client = new SealedHttpClient(new HttpClientHandler()) as IHttpClientActions;
-                await client.DownloadFileAsync(downloadFileUrl, temp);
-
-                await Task.Delay(200);
-
-                var fileInfo = new FileInfo(temp);
-                Assert.IsTrue(fileInfo.Exists);
-
-                var len = fileInfo.Length;
-                len.Should().Be(length);
-
-                var m = fileInfo.MD5();
-                m.Should().Be(md5);
-            }
-            finally
-            {
-                File.Delete(temp);
-            }
-        }
     }
 
     [TestClass]
@@ -294,110 +238,5 @@ namespace SystemNetHttpExtensionsTests
 				File.Delete(expectedFilename);
 			}
 		}
-
-#if !RUN_LIVE_DOWNLOADS
-        [Ignore]
-#endif
-        [TestMethod]
-        public async Task download_small()
-        {
-            var downloadFileUrl = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
-            var length = 13264L;
-            var md5 = "d41d8cd98f00b204e9800998ecf8427e";
-
-            await downloadTest(downloadFileUrl, length, md5);
-        }
-
-#if !RUN_LIVE_DOWNLOADS
-        [Ignore]
-#endif
-        [TestMethod]
-        public async Task download_big()
-        {
-            var downloadFileUrl = "http://www.ovh.net/files/10Mb.dat";
-            var length = 1_250_000L;
-            var md5 = "d41d8cd98f00b204e9800998ecf8427e";
-
-            await downloadTest(downloadFileUrl, length, md5);
-        }
-
-#if !RUN_LIVE_DOWNLOADS
-		[Ignore]
-#endif
-		[TestMethod]
-		public async Task download_small_with_progress()
-		{
-			var downloadFileUrl = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
-			var length = 13264L;
-			var md5 = "d41d8cd98f00b204e9800998ecf8427e";
-
-			var log = new List<string>();
-
-			var progress = new Progress<DownloadProgress>();
-			progress.ProgressChanged += (s, e)
-				=> log.Add(
-					$"{e.BytesReceived}/{e.TotalBytesToReceive} ({e.ProgressPercentage})%"
-					);
-
-			var temp = Path.GetTempFileName();
-			var destinationFilePath = Path.GetFullPath(temp);
-
-			try
-			{
-				// REAL LIVE download client
-				var client = new HttpClient(new HttpClientHandler());
-				await client.DownloadFileAsync(downloadFileUrl, destinationFilePath, progress);
-
-				await Task.Delay(100);
-
-				var fileInfo = new FileInfo(destinationFilePath);
-				Assert.IsTrue(fileInfo.Exists);
-
-				var len = fileInfo.Length;
-				len.Should().Be(length);
-
-				var m = fileInfo.MD5();
-				m.Should().Be(md5);
-
-				log.Count.Should().Be(3);
-				log[0].Should().Be("3685/13264 (27.78)%");
-				log[1].Should().Be("11877/13264 (89.54)%");
-				log[2].Should().Be("13264/13264 (100)%");
-			}
-			finally
-			{
-				File.Delete(destinationFilePath);
-			}
-		}
-
-        private static async Task downloadTest(
-			string downloadFileUrl,
-			long length,
-			string md5)
-        {
-            var temp = Path.GetTempFileName();
-
-            try
-            {
-                // REAL LIVE download client
-                var client = new HttpClient(new HttpClientHandler());
-                await client.DownloadFileAsync(downloadFileUrl, temp);
-
-                await Task.Delay(200);
-
-                var fileInfo = new FileInfo(temp);
-                Assert.IsTrue(fileInfo.Exists);
-
-                var len = fileInfo.Length;
-                len.Should().Be(length);
-
-                var m = fileInfo.MD5();
-                m.Should().Be(md5);
-            }
-            finally
-            {
-                File.Delete(temp);
-            }
-        }
 	}
 }
