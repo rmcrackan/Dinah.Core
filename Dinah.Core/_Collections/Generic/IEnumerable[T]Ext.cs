ee0eafc7288794c7922ec5ae14e64f9180e6ecae
Robert McRackan <rmcrackan@metabolon.com>
2022-04-29 09:18:32 -0400

    New IEnumerable extension method: ToDictionarySafe

--- Dinah.Core/_Collections/Generic/IEnumerable[T]Ext.cs
+++ Dinah.Core/_Collections/Generic/IEnumerable[T]Ext.cs
@@ -4,6 +4,12 @@ using System.Linq;
 
 namespace Dinah.Core.Collections.Generic
 {
+    public enum WinnerEnum
+    {
+        FirstInWins,
+        LastInWins
+    }
+
     public static class IEnumerable_T_Ext
     {
         public static bool In<T>(this T source, params T[] parameters) => _in(source, parameters);
@@ -15,8 +21,40 @@ namespace Dinah.Core.Collections.Generic
 
         /// <summary>Are there any common values between a and b?</summary>
         public static bool SharesAnyValueWith<T>(this IEnumerable<T> a, IEnumerable<T> b)
-            => a == null || b == null
-            ? false
-            : a.Intersect(b).Any();
+            => a is not null
+            && b is not null
+            && a.Intersect(b).Any();
+
+        /// <summary>
+        /// Same as ToDictionary but avoids key conflicts:
+        /// Creates a System.Collections.Generic.Dictionary`2 from an System.Collections.Generic.IEnumerable`1 according to a specified key selector function.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key returned by keySelector.</typeparam>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">An System.Collections.Generic.IEnumerable`1 to create a System.Collections.Generic.Dictionary`2 from.</param>
+        /// <param name="keySelector">A function to extract a key from each element.</param>
+        /// <param name="winner">Whether first in wins or last in wins. Default: first in wins</param>
+        /// <returns>
+        /// A System.Collections.Generic.Dictionary`2 that contains keys and values. The values within each group are in the same order as in source. Key conflicts are safely avoided.
+        /// </returns>
+        public static Dictionary<TKey, TSource> ToDictionarySafe<TKey, TSource>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, WinnerEnum winner = WinnerEnum.FirstInWins)
+        {
+            var dic = new Dictionary<TKey, TSource>();
+
+            if (winner == WinnerEnum.LastInWins)
+                foreach (var value in source)
+                    dic[keySelector(value)] = value;
+            else
+            {
+                foreach (var value in source)
+                {
+                    TKey key = keySelector(value);
+                    if (!dic.ContainsKey(key))
+                        dic[key] = value;
+                }
+            }
+
+            return dic;
+        }
     }
 }
---
+++ _Tests/Dinah.Core.Tests/_Collections/Generic/IEnumerable[T]ExtTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Dinah.Core;
+using Dinah.Core.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+
+namespace IEnumerable_T_ExtTests
+{
+    [TestClass]
+    public class ToDictionarySafe
+    {
+        record Foo(string Bar, string Baz);
+
+        [TestMethod]
+        public void first_wins_default()
+        {
+            var list = new List<Foo>
+            {
+                new("bbb", "111"),
+                new("bbb", "222"),
+            };
+
+            var dic = list.ToDictionarySafe(x => x.Bar);
+            dic.Count.Should().Be(1);
+            dic["bbb"].Baz.Should().Be("111");
+        }
+
+        [TestMethod]
+        public void first_wins_explicit()
+        {
+            var list = new List<Foo>
+            {
+                new("bbb", "111"),
+                new("bbb", "222"),
+            };
+
+            var dic = list.ToDictionarySafe(x => x.Bar, WinnerEnum.FirstInWins);
+            dic.Count.Should().Be(1);
+            dic["bbb"].Baz.Should().Be("111");
+        }
+
+        [TestMethod]
+        public void last_wins()
+        {
+            var list = new List<Foo>
+            {
+                new("bbb", "111"),
+                new("bbb", "222"),
+            };
+
+            var dic = list.ToDictionarySafe(x => x.Bar, WinnerEnum.LastInWins);
+            dic.Count.Should().Be(1);
+            dic["bbb"].Baz.Should().Be("222");
+        }
+
+    }
+}
