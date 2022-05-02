03a6473097c646df3709bd61d66f593c04d7c681
Robert McRackan <rmcrackan@metabolon.com>
2022-05-02 14:16:20 -0400

    Add convenience overload for GetRowIdOfBoundItem

--- Dinah.Core.WindowsDesktop/Windows.Forms/DataGridViewExt.cs
+++ Dinah.Core.WindowsDesktop/Windows.Forms/DataGridViewExt.cs
@@ -10,6 +10,10 @@ namespace Dinah.Core.Windows.Forms
 		public static IEnumerable<DataGridViewRow> AsEnumerable(this DataGridView dataGridView)
 			=> dataGridView.Rows.Cast<DataGridViewRow>();
 
+        public static int GetRowIdOfBoundItem<T>(this DataGridView dataGridView, T item)
+            where T : class
+            => dataGridView.GetRowIdOfBoundItem<T>(x => item == x);
+
         public static int GetRowIdOfBoundItem<T>(this DataGridView dataGridView, Func<T, bool> func)
         {
             for (var r = 0; r < dataGridView.RowCount; r++)
