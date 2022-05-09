8df00e044f101a85c68a16ef8a1baff4aa780e1b
Robert McRackan <rmcrackan@metabolon.com>
2022-05-09 12:56:50 -0400

    Added InterruptableTimer w/demos

---
+++ Dinah.Core/InterruptableTimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Dinah.Core
+{
+    public class InterruptableTimer
+    {
+        public event EventHandler Elapsed;
+
+        private System.Timers.Timer _timer;
+
+        public InterruptableTimer(TimeSpan interval) : this(interval.TotalMilliseconds) { }
+        public InterruptableTimer(double intervalInMilliseconds)
+        {
+            _timer = new() { Interval = intervalInMilliseconds };
+            _timer.Elapsed += PerformNow;
+        }
+
+        /// <summary>
+        /// <para>If timer is not running: invoke event, then begin waiting.</para>
+        /// <para>If timer is running: stop timer (ie: do not wait for interval to complete. aka: interrupt), invoke event then, re-start waiting.</para>
+        /// </summary>
+        public void PerformNow(object _ = null, EventArgs __ = null)
+        {
+            Stop();
+            Elapsed?.Invoke(_, __);
+            _timer.Enabled = true;
+        }
+
+        public void Stop() => _timer.Enabled = false;
+    }
+}
--- _Demos/Dinah.Core.Demos/Program.cs
+++ _Demos/Dinah.Core.Demos/Program.cs
@@ -13,8 +13,10 @@ namespace Dinah.Core.Demos
 
 			//demo.ProgressBarUp();
 			//demo.ProgressBarDown();
-
 			demo.ReadPassword();
+
+			//demo.InterruptableTimer_Blocking();
+			//demo.InterruptableTimer_Async();
 		}
 	}
 	public class Demonstrations
@@ -53,6 +55,76 @@ namespace Dinah.Core.Demos
 			var pw = ConsoleExt.ReadPassword();
 			Console.WriteLine();
 			Console.WriteLine("pw=" + pw);
+		}
+
+		public void InterruptableTimer_Blocking()
+		{
+			/*
+blocking/sequential: 3 sec, 5 sec, 3 sec 5 sec
+
+0: 5/9/2022 12:12:51 PM
+			+1 sec: timer will not start until PerformNow
+1: 5/9/2022 12:12:52 PM
+START: 5/9/2022 12:12:52 PM  <--+
+			+3 sec              |
+END: 5/9/2022 12:12:55 PM       +-- 8 sec
+			+5 sec              |
+START: 5/9/2022 12:13:00 PM  <--+
+			+3 sec
+END: 5/9/2022 12:13:03 PM
+			+5 sec
+START: 5/9/2022 12:13:08 PM
+			 */
+			Console.WriteLine("0: " + DateTime.Now);
+			var t = new InterruptableTimer(5_000);
+
+			// WILL block timer
+			t.Elapsed += (_, __) =>
+			{
+				Console.WriteLine("START: " + DateTime.Now);
+				// if async, it will NOT BLOCK TIMER
+				Task.Delay(3_000).GetAwaiter().GetResult();
+				Console.WriteLine("END: " + DateTime.Now);
+			};
+
+			Thread.Sleep(1_000);
+			Console.WriteLine("1: " + DateTime.Now);
+			t.PerformNow();
+
+			Console.ReadLine();
+		}
+		public void InterruptableTimer_Async()
+		{
+			/*
+non-blocking/async: 3 sec, 2 sec, 3 sec 2 sec
+
+0: 5/9/2022 12:51:36 PM
+1: 5/9/2022 12:51:37 PM
+START: 5/9/2022 12:51:37 PM  <--+
+			+3 sec              |
+END: 5/9/2022 12:51:40 PM       +-- 5 sec
+			+2 sec (5-3)        |
+START: 5/9/2022 12:51:42 PM  <--+
+END: 5/9/2022 12:51:45 PM
+			 */
+			Console.WriteLine("0: " + DateTime.Now);
+			var t = new InterruptableTimer(5_000);
+
+			// will NOT block timer
+			t.Elapsed += async (_, __) =>
+			{
+				Console.WriteLine("START: " + DateTime.Now);
+				// if async, it will NOT BLOCK TIMER
+				await Task.Delay(3_000);
+				Console.WriteLine("END: " + DateTime.Now);
+			};
+
+			// intentionally using Thread.Sleep instead of Task.Delay to show async timer calls from a non-async method
+			Thread.Sleep(1_000);
+			Console.WriteLine("1: " + DateTime.Now);
+			t.PerformNow();
+
+			Console.ReadLine();
 		}
 	}
 }
