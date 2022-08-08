c1799ebb2b7a9108e7e3eeee61ff60f1639f538d
Robert McRackan <rmcrackan@gmail.com>
2022-08-08 11:24:35 -0400

    * ImageReader: icon functions * Windows directories: set/delete folder icon

---
+++ Dinah.Core.WindowsDesktop/Directories.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Dinah.Core.WindowsDesktop
+{
+    // https://github.com/dimuththarindu/FIC-Folder-Icon-Changer/blob/master/project/FIC/Classes/IconCustomizer.cs
+    public static class Directories
+    {
+        public enum FolderTypes { Generic, Documents, Pictures, Music, Videos }
+
+        public static void SetIcon(this DirectoryInfo directoryInfo, string icoPath, FolderTypes folderType = FolderTypes.Generic)
+            => SetIcon(directoryInfo.FullName, icoPath, folderType);
+        public static void SetIcon(string dir, string icoPath, FolderTypes folderType = FolderTypes.Generic)
+        {
+            if (!OperatingSystem.IsWindows()) throw new PlatformNotSupportedException();
+
+            var desktop_ini = Path.Combine(dir, "desktop.ini");
+            var Icon_ico = Path.Combine(dir, "Icon.ico");
+            var hidden = Path.Combine(dir, ".hidden");
+
+            //deleting existing files
+            DeleteIcon(dir);
+
+            //copying Icon file //overwriting
+            File.Copy(icoPath, Icon_ico, true);
+
+            //writing configuration file
+            string[] desktopLines = { "[.ShellClassInfo]", "IconResource=Icon.ico,0", "[ViewState]", "Mode=", "Vid=", $"FolderType={folderType}" };
+            File.WriteAllLines(desktop_ini, desktopLines);
+
+            //configure file 2            
+            string[] hiddenLines = { "desktop.ini", "Icon.ico" };
+            File.WriteAllLines(hidden, hiddenLines);
+
+            //making system files
+            File.SetAttributes(desktop_ini, File.GetAttributes(desktop_ini) | FileAttributes.Hidden | FileAttributes.System | FileAttributes.ReadOnly);
+            File.SetAttributes(Icon_ico, File.GetAttributes(Icon_ico) | FileAttributes.Hidden | FileAttributes.System | FileAttributes.ReadOnly);
+            File.SetAttributes(hidden, File.GetAttributes(hidden) | FileAttributes.Hidden | FileAttributes.System | FileAttributes.ReadOnly);
+
+            //File.SetAttributes(dir, File.GetAttributes(dir) | FileAttributes.ReadOnly);
+
+            refresh();
+        }
+
+        public static void DeleteIcon(this DirectoryInfo directoryInfo) => DeleteIcon(directoryInfo.FullName);
+        public static void DeleteIcon(string dir)
+        {
+            if (!OperatingSystem.IsWindows()) throw new PlatformNotSupportedException();
+
+            var files = new[] { "desktop.ini", "Icon.ico", ".hidden" };
+            foreach (var f in files)
+            {
+                var path = Path.Combine(dir, f);
+                if (!File.Exists(path))
+                    continue;
+
+                File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.Normal);
+                _ = new FileInfo(path) { IsReadOnly = false };
+                File.Delete(path);
+            }
+            refresh();
+        }
+
+        private static void refresh() => SHChangeNotify(0x08000000, 0x0000, (IntPtr)null, (IntPtr)null); //SHCNE_ASSOCCHANGED SHCNF_IDLIST
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern void SHChangeNotify(int wEventId, int uFlags, IntPtr dwItem1, IntPtr dwItem2);
+    }
+}
--- Dinah.Core.WindowsDesktop/Drawing/ImageReader.cs
+++ Dinah.Core.WindowsDesktop/Drawing/ImageReader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 
 namespace Dinah.Core.Drawing
 {
@@ -9,29 +10,70 @@ namespace Dinah.Core.Drawing
 	/// </summary>
 	public static class ImageReader
     {
-        // these seemingly intuitive ways of loading an image will lock the file
+        // these seemingly intuitive ways of loading an image can lock the file
         // - with a stream
         // - Image.FromFile()
         // use this method instead: https://stackoverflow.com/a/8701772
         public static Image ToImage(string filepath)
         {
-			using var bmpTemp = new Bitmap(filepath);
-			return new Bitmap(bmpTemp);
-		}
+            using var bmpTemp = new Bitmap(filepath);
+            return new Bitmap(bmpTemp);
+        }
 
-		public static Image ToImage(byte[] bytes)
-		{
-			using var memoryStream = new System.IO.MemoryStream(bytes);
-			return Image.FromStream(memoryStream);
-		}
+        public static Image ToImage(byte[] bytes)
+        {
+            using var memoryStream = new MemoryStream(bytes);
+            return Image.FromStream(memoryStream);
+        }
 
-		public static byte[] ToBytes(this Image image, ImageFormat format)
-		{
-			using var ms = new System.IO.MemoryStream();
-			{
-				image.Save(ms, format);
-				return ms.ToArray();
-			}
-		}
-	}
+        public static byte[] ToBytes(this Image image, ImageFormat format)
+        {
+            using var ms = new MemoryStream();
+            image.Save(ms, format);
+            return ms.ToArray();
+        }
+
+        public static Icon ToIcon(string filepath) => ToImage(filepath).ToIcon();
+
+        // https://stackoverflow.com/a/21389253
+        public static Icon ToIcon(this Image img)
+        {
+            using var ms = new MemoryStream();
+            using var bw = new BinaryWriter(ms);
+            // Header
+            bw.Write((short)0);   // 0-1 : reserved
+            bw.Write((short)1);   // 2-3 : 1=ico, 2=cur
+            bw.Write((short)1);   // 4-5 : number of images
+                                  // Image directory
+            var w = img.Width;
+            if (w >= 256) w = 0;
+            bw.Write((byte)w);    // 0 : width of image
+            var h = img.Height;
+            if (h >= 256) h = 0;
+            bw.Write((byte)h);    // 1 : height of image
+            bw.Write((byte)0);    // 2 : number of colors in palette
+            bw.Write((byte)0);    // 3 : reserved
+            bw.Write((short)0);   // 4 : number of color planes
+            bw.Write((short)0);   // 6 : bits per pixel
+            var sizeHere = ms.Position;
+            bw.Write((int)0);     // 8 : image size
+            var start = (int)ms.Position + 4;
+            bw.Write(start);      // 12: offset of image data
+                                  // Image data
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            var imageSize = (int)ms.Position - start;
+            ms.Seek(sizeHere, SeekOrigin.Begin);
+            bw.Write(imageSize);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            // And load it
+            return new Icon(ms);
+        }
+
+        public static void Save(this Icon icon, string path)
+        {
+            using var fs = new FileStream(path, FileMode.Create);
+            icon.Save(fs);
+        }
+    }
 }
