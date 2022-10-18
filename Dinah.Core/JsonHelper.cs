93b22062b55e63eac9d6015c28ef7f39933cef6b
Robert McRackan <rmcrackan@gmail.com>
2022-10-18 07:40:00 -0400

    New JsonHelper functions

--- Dinah.Core/JsonHelper.cs
+++ Dinah.Core/JsonHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -25,5 +26,44 @@ namespace Dinah.Core
 
 			return instance;
 		}
-	}
+
+        /// <summary>
+        /// Find a key containing all of these strings. Recursive search
+        /// </summary>
+        public static JProperty Search(string jsonFilePath, IEnumerable<string> searchPieces)
+        {
+            // SingleOrDefault: don't accidentally update more than intended
+            var contents = File.ReadAllText(jsonFilePath);
+            var result = JObject.Parse(contents)
+                .AllTokens()
+                .Where(a => a is JProperty)
+                .Cast<JProperty>()
+                .SingleOrDefault(prop => searchPieces.All(piece => prop.Name.ContainsInsensitive(piece)));
+            return result;
+        }
+
+        /// <summary>
+        /// Especially convenient when used with linq
+        /// </summary>
+        public static IEnumerable<JToken> AllTokens(this JObject obj)
+        {
+            var toSearch = new Stack<JToken>(obj.Children());
+            while (toSearch.Count > 0)
+            {
+                var inspected = toSearch.Pop();
+                yield return inspected;
+
+                foreach (var child in inspected)
+                    toSearch.Push(child);
+            }
+        }
+
+        public static void UpdateJsonFile(string jsonFilePath, Action<JObject> updateAction)
+        {
+            var contents = File.ReadAllText(jsonFilePath);
+            var jObj = JObject.Parse(contents);
+            updateAction(jObj);
+            File.WriteAllText(jsonFilePath, jObj.ToString(Formatting.Indented));
+        }
+    }
 }
