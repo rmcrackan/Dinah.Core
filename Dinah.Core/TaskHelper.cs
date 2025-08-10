1caedcb67c21f7e11dc28be40e2d49ec73a35530
rmcrackan <rmcrackan@gmail.com>
2025-08-10 10:59:58 -0400

    Multi-return WhenAll helpers

---
+++ Dinah.Core/TaskHelper.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Dinah.Core
+{
+    // Inspired by https://old.reddit.com/r/csharp/comments/1mkrlcc/what_is_the_lowest_effort_highest_impact_helper/n7le5cg/
+    public static class TaskHelper
+    {
+        #region WhenAll<T1, ..., T10>
+        public static async Task<T1> WhenAll<T1>(Task<T1> t1)
+        {
+            await Task.WhenAll(t1);
+            return (t1.Result);
+        }
+
+        public static async Task<(T1, T2)> WhenAll<T1, T2>(Task<T1> t1, Task<T2> t2)
+        {
+            await Task.WhenAll(t1, t2);
+            return (t1.Result, t2.Result);
+        }
+
+        public static async Task<(T1, T2, T3)> WhenAll<T1, T2, T3>(Task<T1> t1, Task<T2> t2, Task<T3> t3)
+        {
+            await Task.WhenAll(t1, t2, t3);
+            return (t1.Result, t2.Result, t3.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4)> WhenAll<T1, T2, T3, T4>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4)
+        {
+            await Task.WhenAll(t1, t2, t3, t4);
+            return (t1.Result, t2.Result, t3.Result, t4.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5)> WhenAll<T1, T2, T3, T4, T5>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6)> WhenAll<T1, T2, T3, T4, T5, T6>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5, t6);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7)> WhenAll<T1, T2, T3, T4, T5, T6, T7>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5, t6, t7);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8)> WhenAll<T1, T2, T3, T4, T5, T6, T7, T8>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7, Task<T8> t8)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5, t6, t7, t8);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result, t8.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9)> WhenAll<T1, T2, T3, T4, T5, T6, T7, T8, T9>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7, Task<T8> t8, Task<T9> t9)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result, t8.Result, t9.Result);
+        }
+
+        public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)> WhenAll<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7, Task<T8> t8, Task<T9> t9, Task<T10> t10)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result, t8.Result, t9.Result, t10.Result);
+        }
+        #endregion
+
+        #region using tuples and ducktyping. see test: Dinah.Core.Tests.WhenAll.ducktyping
+        public static TaskAwaiter<T1> GetAwaiter<T1>(this Task<T1> task) => WhenAll(task).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2)> GetAwaiter<T1, T2>(this (Task<T1>, Task<T2>) tasks) => WhenAll(tasks.Item1, tasks.Item2).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2, T3)> GetAwaiter<T1, T2, T3>(this (Task<T1>, Task<T2>, Task<T3>) tasks) => WhenAll(tasks.Item1, tasks.Item2, tasks.Item3).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2, T3, T4)> GetAwaiter<T1, T2, T3, T4>(this (Task<T1>, Task<T2>, Task<T3>, Task<T4>) tasks) => WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5)> GetAwaiter<T1, T2, T3, T4, T5>(this (Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>) tasks) => WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5, T6)> GetAwaiter<T1, T2, T3, T4, T5, T6>(this (Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>, Task<T6>) tasks) => WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5, tasks.Item6).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5, T6, T7)> GetAwaiter<T1, T2, T3, T4, T5, T6, T7>(this (Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>, Task<T6>, Task<T7>) tasks) => WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5, tasks.Item6, tasks.Item7).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5, T6, T7, T8)> GetAwaiter<T1, T2, T3, T4, T5, T6, T7, T8>(this (Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>, Task<T6>, Task<T7>, Task<T8>) tasks) => WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5, tasks.Item6, tasks.Item7, tasks.Item8).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5, T6, T7, T8, T9)> GetAwaiter<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this (Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>, Task<T6>, Task<T7>, Task<T8>, Task<T9>) tasks) => WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5, tasks.Item6, tasks.Item7, tasks.Item8, tasks.Item9).GetAwaiter();
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)> GetAwaiter<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this (Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>, Task<T6>, Task<T7>, Task<T8>, Task<T9>, Task<T10>) tasks) => WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5, tasks.Item6, tasks.Item7, tasks.Item8, tasks.Item9, tasks.Item10).GetAwaiter();
+
+
+
+        #endregion
+    }
+}
---
+++ _Tests/Dinah.Core.Tests/TaskHelperTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Dinah.Core.Tests
+{
+    [TestClass]
+    public class WhenAll
+    {
+        private async Task<int> Task1Async() => await Task.FromResult(1);
+        private async Task<int> Task2Async() => await Task.FromResult(2);
+        private async Task<int> Task3Async() => await Task.FromResult(3);
+
+        [TestMethod]
+        public async Task mult_returns()
+        {
+            var (result1, result2, result3) = await TaskHelper.WhenAll(Task1Async(), Task2Async(), Task3Async());
+            Assert.AreEqual(1, result1);
+            Assert.AreEqual(2, result2);
+            Assert.AreEqual(3, result3);
+        }
+
+        [TestMethod]
+        public async Task ducktyping()
+        {
+            var (result1, result2, result3) = await (
+                Task1Async(),
+                Task2Async(),
+                Task3Async());
+            Assert.AreEqual(1, result1);
+            Assert.AreEqual(2, result2);
+            Assert.AreEqual(3, result3);
+        }
+    }
+}
